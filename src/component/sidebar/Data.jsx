7179204a0a2861b56550188c8aae353ee3720214
soban-mahmood <sobanmehmood54@gmail.com>
2024-07-31 16:08:15 +0500

    .

---
+++ src/component/sidebar/Data.jsx
@@ -0,0 +1,34 @@
+import {
+    
+    UilEstate,
+    UilClipboardAlt,
+    UilUsersAlt,
+    UilChart,
+    UilSignOutAlt,
+    UilPackage
+    
+} from "@iconscout/react-unicons"
+
+
+export const SidebarData = [
+    {
+        icons:UilEstate,
+        heading:'Dashboard'
+    },
+    {
+        icons:UilClipboardAlt,
+        heading:'Orders'
+    },
+    {
+        icons:UilUsersAlt,
+        heading:'Customers'
+    },
+    {
+        icons:UilPackage,
+        heading:'Product'
+    },
+    {
+        icons:UilChart,
+        heading:'Analytics'
+    }
+]
--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -1,15 +1,27 @@
 import React from "react";
-import logo from "../../../public/logo.png"
-import './Sidebar.css'
+import logo from "../../../public/logo.png";
+import "./Sidebar.css";
+import { UilEstate } from "@iconscout/react-unicons";
+import { SidebarData } from "./Data";
 function Sidebar() {
-
   return (
     <div>
       <div className="Sidebar">
         <div className="logo">
           <img src={logo} alt="" />
-          Sh<span>o</span> p
+          <span>
+            Sh<span>o</span>pe
+          </span>
         </div>
+        {/* Menu */}
+        <div className="menu">
+  {SidebarData?.map((item, index) => (
+    <div className="menuItem">
+      <item.icons />
+      <span>{item.heading}</span>
+    </div>
+  ))}
+</div>
       </div>
     </div>
   );
